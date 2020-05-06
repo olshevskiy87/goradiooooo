f1f8aa23a4a458b73a6c621d1247a9de1a2dbc97
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2020-05-06 19:43:42 +0300

    do not allow more than one decade or country for mode "explore"

--- params/params.go
+++ params/params.go
@@ -14,11 +14,13 @@ type Params struct {
 }
 
 func New(pMode string, pMoods []string, pDecades []Decade, pCountries []string) (*Params, error) {
+	// mode
 	mode, err := canonicalizeMode(pMode)
 	if err != nil {
 		return nil, fmt.Errorf("could not canonicalize mode \"%s\": %v", pMode, err)
 	}
 
+	// moods
 	var moods []string
 	if len(pMoods) == 0 {
 		moods = []string{MOOD_SLOW, MOOD_FAST, MOOD_WEIRD}
@@ -29,18 +31,26 @@ func New(pMode string, pMoods []string, pDecades []Decade, pCountries []string) 
 		}
 	}
 
+	// countries
+	if mode == MODE_EXPLORE && len(pCountries) > 1 {
+		return nil, fmt.Errorf("more than one country specified for mode \"%s\" (use \"%s\")", mode, MODE_TAXI)
+	}
 	countries, err := canonicalizeCountries(pCountries)
 	if err != nil {
 		return nil, fmt.Errorf("could not canonicalize countries codes %v: %v", pCountries, err)
 	}
 
+	// decades
+	if mode == MODE_EXPLORE && len(pDecades) > 1 {
+		return nil, fmt.Errorf("more than one decade specified for mode \"%s\" (use \"%s\")", mode, MODE_TAXI)
+	}
 	decades, err := canonicalizeDecades(pDecades)
 	if err != nil {
 		return nil, fmt.Errorf("could not canonicalize decades %v: %v", pDecades, err)
 	}
 
 	if (mode == MODE_TAXI || mode == MODE_EXPLORE) && (len(decades) == 0 || len(countries) == 0) {
-		return nil, fmt.Errorf("decades and countries must be specified for mode \"%s\"", pMode)
+		return nil, fmt.Errorf("decades and countries must be specified for mode \"%s\"", mode)
 	}
 
 	return &Params{
