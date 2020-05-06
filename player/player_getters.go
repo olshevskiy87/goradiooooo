a918a7ad6bd14593c6778ce6e07ad390c0157c20
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2020-05-06 20:14:37 +0300

    refactor func GetNextSongLink()

--- player/player_getters.go
+++ player/player_getters.go
@@ -6,33 +6,26 @@ import (
 	"net/http"
 )
 
-func (r *RadioooooPlayer) GetNextSongLink() (*Song, error) {
+func (r *RadioooooPlayer) requestSongInfo() (string, error) {
 	payload, err := r.params.GetRequestPayload()
 	if err != nil {
-		return nil, fmt.Errorf("could not prepare request payload: %v", err)
+		return "", fmt.Errorf("could not prepare request payload: %v", err)
 	}
-
 	response, responseBody, errs := r.requestAgent.
 		Post(r.url).
 		Send(payload).
 		End()
 	if errs != nil {
-		return nil, fmt.Errorf("could not perform request with url \"%s\": %v", r.url, errs)
+		return "", fmt.Errorf("could not perform request with url \"%s\": %v", r.url, errs)
 	}
 	if response.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("could not perform request with url \"%s\" (status %d): %v", r.url, response.StatusCode, errs)
+		return "", fmt.Errorf("could not perform request with url \"%s\" (status %d): %v", r.url, response.StatusCode, errs)
 	}
+	return responseBody, nil
+}
 
-	var songInfo map[string]interface{}
-	err = json.Unmarshal([]byte(responseBody), &songInfo)
-	if err != nil {
-		return nil, fmt.Errorf("could not parse json: %v", err)
-	}
-	if errorMsg, ok := songInfo["error"]; ok {
-		return nil, &ErrorInJSON{errorMsg.(string)}
-	}
-
-	links, ok := songInfo["links"]
+func (r *RadioooooPlayer) makeSong(info map[string]interface{}) (*Song, error) {
+	links, ok := info["links"]
 	if !ok {
 		return nil, fmt.Errorf("no key \"links\" in song info")
 	}
@@ -49,24 +42,41 @@ func (r *RadioooooPlayer) GetNextSongLink() (*Song, error) {
 	for format, link := range linksStr {
 		song.Links[format] = link.(string)
 	}
-	if artist, ok := songInfo["artist"]; ok {
+	if artist, ok := info["artist"]; ok {
 		song.Artist = artist.(string)
 	}
-	if album, ok := songInfo["album"]; ok {
+	if album, ok := info["album"]; ok {
 		song.Album = album.(string)
 	}
-	if title, ok := songInfo["title"]; ok {
+	if title, ok := info["title"]; ok {
 		song.Title = title.(string)
 	}
-	if year, ok := songInfo["year"]; ok {
+	if year, ok := info["year"]; ok {
 		song.Year = year.(string)
 	}
-	if country, ok := songInfo["country"]; ok {
+	if country, ok := info["country"]; ok {
 		song.Country = country.(string)
 	}
-	if mood, ok := songInfo["mood"]; ok {
+	if mood, ok := info["mood"]; ok {
 		song.Mood = mood.(string)
 	}
-
 	return song, nil
+}
+
+func (r *RadioooooPlayer) GetNextSongLink() (*Song, error) {
+	songResponse, err := r.requestSongInfo()
+	if err != nil {
+		return nil, fmt.Errorf("could not request song info: %v", err)
+	}
+
+	var songInfo map[string]interface{}
+	err = json.Unmarshal([]byte(songResponse), &songInfo)
+	if err != nil {
+		return nil, fmt.Errorf("could not parse json with song info: %v", err)
+	}
+	if errorMsg, ok := songInfo["error"]; ok {
+		return nil, &ErrorInJSON{errorMsg.(string)}
+	}
+
+	return r.makeSong(songInfo)
 }
