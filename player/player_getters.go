093fc88c0bc502da3a5ee73e922ef6e348ddec5d
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2020-05-07 20:39:14 +0300

    check if json value is nil in requested song info

--- player/player_getters.go
+++ player/player_getters.go
@@ -29,6 +29,9 @@ func (r *RadioooooPlayer) makeSong(info map[string]interface{}) (*Song, error) {
 	if !ok {
 		return nil, fmt.Errorf("no key \"links\" in song info")
 	}
+	if links == nil {
+		return nil, fmt.Errorf("empty \"links\" in song info")
+	}
 
 	linksStr := links.(map[string]interface{})
 	linksLen := len(linksStr)
@@ -40,24 +43,26 @@ func (r *RadioooooPlayer) makeSong(info map[string]interface{}) (*Song, error) {
 		Links: make(map[string]string, linksLen),
 	}
 	for format, link := range linksStr {
-		song.Links[format] = link.(string)
+		if link != nil {
+			song.Links[format] = link.(string)
+		}
 	}
-	if artist, ok := info["artist"]; ok {
+	if artist, ok := info["artist"]; ok && artist != nil {
 		song.Artist = artist.(string)
 	}
-	if album, ok := info["album"]; ok {
+	if album, ok := info["album"]; ok && album != nil {
 		song.Album = album.(string)
 	}
-	if title, ok := info["title"]; ok {
+	if title, ok := info["title"]; ok && title != nil {
 		song.Title = title.(string)
 	}
-	if year, ok := info["year"]; ok {
+	if year, ok := info["year"]; ok && year != nil {
 		song.Year = year.(string)
 	}
-	if country, ok := info["country"]; ok {
+	if country, ok := info["country"]; ok && country != nil {
 		song.Country = country.(string)
 	}
-	if mood, ok := info["mood"]; ok {
+	if mood, ok := info["mood"]; ok && mood != nil {
 		song.Mood = mood.(string)
 	}
 	return song, nil
@@ -74,8 +79,14 @@ func (r *RadioooooPlayer) GetNextSong() (*Song, error) {
 	if err != nil {
 		return nil, fmt.Errorf("could not parse json with song info: %v", err)
 	}
-	if errorMsg, ok := songInfo["error"]; ok {
-		return nil, &ErrorInJSON{errorMsg.(string)}
+	if errorValue, ok := songInfo["error"]; ok {
+		var msg string
+		if errorValue != nil {
+			msg = errorValue.(string)
+		} else {
+			msg = "unknow error"
+		}
+		return nil, &ErrorInJSON{msg}
 	}
 
 	return r.makeSong(songInfo)
