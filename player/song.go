a096fbb0ab21ebc74c123b46998225f9194eaf4a
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2020-05-28 19:18:27 +0300

    show full country name instead of ISO code

--- player/song.go
+++ player/song.go
@@ -1,6 +1,10 @@
 package player
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/olshevskiy87/goradiooooo/params"
+)
 
 type Song struct {
 	Links   map[string]string
@@ -16,7 +20,11 @@ func (s *Song) String() string {
 	artist := "-"
 	if s.Artist != "" {
 		if s.Country != "" {
-			artist = fmt.Sprintf("%s (%s)", s.Artist, s.Country)
+			countryName, err := params.GetCountryName(s.Country)
+			if err != nil {
+				countryName = s.Country
+			}
+			artist = fmt.Sprintf("%s (%s)", s.Artist, countryName)
 		} else {
 			artist = s.Artist
 		}
